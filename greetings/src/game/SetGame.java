46d28cbd12e6c147e365ea066e7c9081d840ec20
djpiz <djpiz@DESKTOP-G6SQLNA>
2017-03-20 14:32:21 +0100

    asd

---
+++ greetings/src/game/SetGame.java
@@ -0,0 +1,46 @@
+package game;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SetGame
+ */
+@WebServlet("/setgame")
+public class SetGame extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private static final int DEFAULT_MAX_TRIAL = 5;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SetGame() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String word = (request.getParameter("word")==null) ? "default" :
+						request.getParameter("word");
+		String tryNo = (request.getParameter("tryNo")==null) ? "" :
+			request.getParameter("tryNo");
+		
+		int maxTrial;
+		try {
+			maxTrial = Integer.parseInt(tryNo);
+		} catch(NumberFormatException ex) {
+			maxTrial = DEFAULT_MAX_TRIAL;
+		}
+		
+	}
+
+	
+
+}
